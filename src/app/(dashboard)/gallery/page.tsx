eed50f2834a697f73f68a2c5621f2363ce78be52
PGCwebdesigns <108600974+ThePGCoder@users.noreply.github.com>
2025-05-03 10:09:19 +1200

    first commit

---
+++ src/app/(dashboard)/gallery/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface GalleryProps {
+}
+
+const Gallery: React.FC<GalleryProps> = () => {
+    return (
+        <>
+            Gallery
+        </>
+    );
+};
+
+export default Gallery;
---
+++ src/app/(dashboard)/the-team/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface TheTeamProps {}
+
+const TheTeam: React.FC<TheTeamProps> = () => {
+  return <>TheTeam</>;
+};
+
+export default TheTeam;
